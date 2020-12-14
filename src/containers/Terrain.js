07ebd343195fef38cce8fce4f5dbe4e7198bea55
Dawid Szpunar <szpunardawid@gmail.com>
2020-12-14 22:30:49 +0100

    add skybox

--- src/containers/Terrain.js
+++ src/containers/Terrain.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {Canvas, extend, useFrame} from 'react-three-fiber'
+import {Canvas, extend, useFrame, useThree} from 'react-three-fiber'
 import Ground from "../components/Ground";
 import Box from "../components/Box";
 import CameraControls from "../components/CameraControls"
-import { Stars } from '@react-three/drei'
+import { Sky, Stars } from '@react-three/drei'
 import {Physics} from "use-cannon"
+import { CubeTextureLoader } from "three";
 
-
+function Skybox() {
+    const {scene} = useThree()
+    const loader = new CubeTextureLoader();
+    const texture = loader.load([
+        "/1.jpg",
+        "/2.jpg",
+        "/3.jpg",
+        "/4.jpg",
+        "/5.jpg",
+        "/6.jpg",
+    ]);
+    scene.background = texture
+    return null
+}
 
 export default function Terrain(){
     return(///x,y,z    - z głebia
@@ -14,13 +28,14 @@ export default function Terrain(){
             <CameraControls />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
+            <directionalLight intensity={0.5}/> 
             <Stars />
             <ambientLight intensity={0.1}/>
             <Physics>
                 <Box />
                 <Ground position={[100, 3, 0]} />
             </Physics>
-            
+            <Skybox />
         </Canvas>
     )
 }
